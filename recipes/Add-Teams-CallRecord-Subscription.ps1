2170c3201a9904a3d1c3d0289a838f6d07be381c
Glenn <glenn.ferrie@gmail.com>
2025-01-16 21:52:55 -0500

    Add files via upload

---
+++ recipes/Add-Teams-CallRecord-Subscription.ps1
@@ -0,0 +1,39 @@
+﻿# Load CSharp Assembly for HttpUtility
+Add-Type -AssemblyName System.Web
+
+$client_state = "bellmore-code-telemetry-callrecords"
+$notifyUrl = "https://gfdatafunx.azurewebsites.net/api/subs/callRecords/hook?code=CcZmuUjYN1jsjeJWqGX1De6OXN4Kv3JMV078geg1pnJZtCYJWRYaPA=="
+$lifecycleUrl = "https://gfdatafunx.azurewebsites.net/api/subs/callRecords/feed/refresh?code=LfH5lIKBY1HCjyxdjJAPcb2dQozXIj0PhzPy9AaRyy6z8tnVIGXteg=="
+
+# resourceURL
+$subs_url = "https://graph.microsoft.com/v1.0/subscriptions"
+# scope
+$scope = [System.Web.HttpUtility]::UrlEncode("https://graph.microsoft.com/.default")
+
+# auth details
+$apikey = "186753e1-5f9c-4084-99b6-81040a4eaba6"
+$secret = [System.Web.HttpUtility]::UrlEncode("z6YK.~K1zxuQi~W1b-6969Qgg.mXO_hrR3")
+$tenant = "e16e56f2-9747-40a6-8189-09ba0f1fa61c"
+
+# request preparation
+$auth_url = "https://login.microsoftonline.com/$tenant/oauth2/v2.0/token"
+$auth_body = "client_id=$apikey&client_secret=$secret&scope=$scope&grant_type=client_credentials"
+
+# make authorization request
+$resp = Invoke-WebRequest -UseBasicParsing -Uri $auth_url -Method "POST" -Body $auth_body
+# get auth token from response
+$respObj = ConvertFrom-Json $resp.Content
+# create bearer token
+$dict = @{"Authorization"="Bearer $($respObj.access_token)";"Content-Type"="application/json"}
+
+# make subscription request object
+$body = "{""changeType"":""created,updated"",""lifecycleNotificationUrl"":""$lifecycleUrl"",""notificationUrl"":""$notifyUrl"",""resource"":""/communications/callRecords"",""expirationDateTime"":""2021-06-24T23:59:59.9999999Z"", ""clientState"":""$client_state""}"
+
+# send subscription request
+$resp2 = Invoke-WebRequest -UseBasicParsing -Uri $subs_url -Method "POST" -Body $body -Headers $dict
+
+# show result
+Write-Host $resp2.Content
+
+https://gfdatafunx.azurewebsites.net/api/subs/callRecords/hook?code=CcZmuUjYN1jsjeJWqGX1De6OXN4Kv3JMV078geg1pnJZtCYJWRYaPA==
+
---
+++ recipes/Allow Application Access TeamsMeeting.ps1
@@ -0,0 +1,50 @@
+﻿Function BlankSpace {
+    param([int]$howmany = 1)
+    #Write-Host $howmany
+    Write-Host " "
+    if ($howmany -ge 2) {
+        BlankSpace -howmany ($howmany-1)
+    }
+}
+Function CheckPSVersion {
+
+    param([int]$major,[int]$minor)
+
+    $version_table = $PSVersionTable.PSVersion
+    if ($version_table.Major -ge $major -and $version_table.Minor -ge $minor) {
+        WRite-Host "PowerShell Version is OK" -ForegroundColor Green
+        return $true
+    } else {
+        WRite-Host "PowerShell is out-of-date" -ForegroundColor DarkRed
+        return $false
+    }
+}
+
+BlankSpace 
+WRite-Host "Start Time: $([System.DateTime]::Now)"
+WRite-Host "Setup AppRegistration Policy for Teams OnlineMeetings" -ForegroundColor Yellow
+
+# check if we are on PS 5.1 or better.
+$res = CheckPSVersion -major 5 -minor 1
+
+if ($res) {
+    
+    ## Install PSGet :: PowerShell Package Manager
+    Write-Host "Install PSGet :: PowerShell Package Manager" -ForegroundColor Magenta
+    Install-Module -Name PowerShellGet -Force -AllowClobber -ErrorAction Continue
+    
+    ## Install Microsoft Teams / Skype Module
+    Write-Host "Install Microsoft Teams / Skype Module" -ForegroundColor Magenta
+    Import-Module MicrosoftTeams -Force -AllowClobber -ErrorAction Continue 
+    Write-Host "Modules imported.... " -ForegroundColor Green
+
+    # Connect to Teams
+    $tenantid = "e16e56f2-9747-40a6-8189-09ba0f1fa61c"
+    Connect-MicrosoftTeams 
+
+    Get-CsApplicationAccessPolicy
+}
+
+# hasta la vista ~~
+WRite-Host "Good Bye!" -ForegroundColor Cyan
+BlankSpace
